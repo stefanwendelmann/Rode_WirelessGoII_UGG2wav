f1650b359d8410186837b8a39e91b1d0c4e64407
Stefan Höltker <SHoeltker@itout.de>
2022-02-16 20:47:32 +0100

    main: added requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RodeWirelessGo2UGG2wav',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='stefanhoeltker',
+    author_email='',
+    description='Convert UGG Files Created with the Rode Wireless GO 2 to wav'
+)
